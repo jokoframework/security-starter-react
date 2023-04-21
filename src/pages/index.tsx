5e05fdd578253dedd0f531ad757970efab5fcbd4
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-21 19:18:24 -0400

    Empezar layout principal con Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="grid grid-rows-3 grid-flow-col gap-4">
+      <div className="max-sm:hidden row-span-3 bg-blue-950">Barra de navegacion</div>
+      <div className="col-span-2">Cabecera</div>
+      <div className="row-span-2 col-span-2 bg-neutral-400">
+        <h1 className="text-3xl font-bold underline">
+          Bienvenido a Joko Security.
+        </h1>
+      </div>
+    </div>
+    </>
   )
 }
