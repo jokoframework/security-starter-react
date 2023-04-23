c0bbe7f02da25474b6fa83a7f0b14619353303ae
Alain Vega <alainjosevz@gmail.com>
2023-04-23 17:42:52 -0400

    Cree un link desde index a login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p><Link href="login" className="hover:underline">Click here </Link>to login!</p>
+    </>
   )
 }
