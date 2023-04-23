41387e583a3a241da04e05d4a8036fdab9b98070
Alain Vega <alainjosevz@gmail.com>
2023-04-23 17:38:57 -0400

    Mejoras esteticas al formulario y a la pagina

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link"
-import { useEffect } from "react";
 //Pagina de login para los usuarios.
 export default function Login() {
     return ( 
         <> {/* TODO: Usar variables de css (no hay comentarios semanticos, bajon)*/}
-            <div className="min-h-screen min-w-full mx-auto my-auto flex flex-col justify-center items-center bg-slate-900 text-white gap-4">
-                <form className="border flex flex-col justify-center items-center bg-slate-700 p-6 gap-4 rounded-3xl">
+            <div className="min-h-screen min-w-full mx-auto my-auto flex flex-col justify-center items-center bg-slate-900 text-white gap-4 font-serif">
+                <form className="border flex flex-col justify-center items-center bg-slate-700 p-6 gap-4 rounded-3xl"> {/* TODO: ver donde enviar esto */}
                     <h1 className="italic text-3xl">
                         Inicie sesion
                     </h1>
@@ -17,12 +16,12 @@ export default function Login() {
                     </label>
                     <button type="submit" className="bg-gray-400 rounded-2xl p-2 min-w-full">Iniciar</button>
                     <div className="flex gap-5">
-                        <p>Olvido su contraseña?</p> {/* TODO: preguntar si me tengo que preocupar por esto. */}
-                        <p><Link href="/">Registrarse</Link></p> {/* TODO: crear pagina para registrarse. */}
+                        <p className="hover:underline cursor-pointer" >Olvido su contraseña?</p> {/* TODO: preguntar si me tengo que preocupar por esto. */}
+                        <p className="hover:underline"><Link href="/">Registrarse</Link></p> {/* TODO: crear pagina para registrarse. */}
                     </div>
                 </form>
                     <p>
-                        <Link href="/" className="hover:font-extrabold">Click here</Link> to go back {/* TODO: lo mas probable es que esto tenga que chutar */}
+                        <Link href="/" className="hover:font-extrabold">Click aqui</Link> para ir a la pagina principal {/* TODO: lo mas probable es que esto tenga que chutar */}
                     </p>
             </div>
         </>
