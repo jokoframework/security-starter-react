6e8b9d0ece228fefff309ee02f9994ff7b969eee
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-25 01:27:41 -0400

    Agregar TODO y cambios de cursor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,30 +11,42 @@ export default function Home() {
     <div className="grid grid-rows-12 grid-cols-12">
       <div className="max-sm:hidden row-span-4 row-start-1 bg-blue-900 overflow-y-auto col-span-2 py-3 text-slate-300">
         <ul className="w-full">
-          <li className="py-2 mx-2 rounded-md hover:bg-blue-500">
-            <a className="px-4 text-lg hover:text-slate-100" href="#">
-              Barra de navegacion
+          <li className="py-2 mx-2 font-semibold text-center">
+            <a className="px-4 text-xl hover:text-slate-50" href="#">
+              Joko Security
             </a>
           </li>
-          <li className="py-2 mx-2 rounded-md hover:bg-blue-500">
-           <a className="px-4 text-lg hover:text-slate-100" href="#">
+          <li className="hover:cursor-pointer py-2 mx-2 rounded-md hover:bg-blue-500 hover:text-slate-50">
+           <a className="px-4 text-lg" href="#">
               Item 1
             </a>
           </li>
-          <li className="py-2 mx-2 rounded-md hover:bg-blue-500">
-            <a className="px-4 text-lg hover:text-slate-100" href="#">
+          <li className="hover:cursor-pointer py-2 mx-2 rounded-md hover:bg-blue-500 hover:text-slate-100">
+            <a className="px-4 text-lg" href="#">
               Item 2
             </a>
           </li>
         </ul>
       </div>
-      <div className="max-sm:col-span-12 col-span-10 px-3 py-3">
-        <div>Cabecera (falta boton)</div>
+      <div className="max-sm:col-span-12 col-span-10 px-3 py-4">
+        <div>Cabecera</div>
       </div>
       <div className="row-span-3 max-sm:col-span-12 col-span-10 bg-neutral-100">
-        <h1 className="m-4 p-4 text-3xl h-screen font-bold bg-white">
-          Bienvenido a Joko Security.
-        </h1>
+        <div className="m-4 p-4  h-screen bg-white">
+          <p className="text-3xl font-bold">Bienvenido a Joko Security.</p>
+          <h1 className="text-3xl">TODO:</h1>
+          <ul>
+            <li>
+              Iconos para los items en el navbar.
+            </li>
+            <li>
+              Boton en la cabecera para esconder y hacer aparecer el navbar.
+            </li>
+            <li>
+              Hacer que el layout sea un layout de verdad (aplicarlo a diferentes rutas).
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     </>
