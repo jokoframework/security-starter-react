38f6468fa75e4e52281ffabc7b38428607d30542
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-25 01:32:12 -0400

    Agregar algo al TODO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,9 @@ export default function Home() {
             <li>
               Hacer que el layout sea un layout de verdad (aplicarlo a diferentes rutas).
             </li>
+            <li>
+              Hacer que los items en el navbar sean clickeables en lugar de solo el texto.
+            </li>
           </ul>
         </div>
       </div>
