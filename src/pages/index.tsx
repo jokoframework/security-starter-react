c85a8381c5defcf504481fb8115b4121a1fe220d
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-24 23:53:49 -0400

    Agregar navbar vertical
    Actualmente el navbar solo aparece desde el breakpoint max-sm.
    TODO: Agregar un boton para hacerlo colapsar al clickear.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,32 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <div className="grid grid-rows-3 grid-flow-col gap-4">
-      <div className="max-sm:hidden row-span-3 bg-blue-950">Barra de navegacion</div>
-      <div className="col-span-2">Cabecera</div>
-      <div className="row-span-2 col-span-2 bg-neutral-400">
-        <h1 className="text-3xl font-bold underline">
+    <div className="grid grid-rows-3 grid-flow-col">
+      <div className="max-sm:hidden row-span-3 bg-blue-950 overflow-y-auto col-span-1 flex items-center text-slate-300">
+        <ul>
+          <li>
+            <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+              Barra de navegacion
+            </a>
+          </li>
+          <li>
+           <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+              Item 1
+            </a>
+          </li>
+          <li>
+            <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+              Item 2
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-span-11">
+        <div className=""></div>
+        <div>Cabecera</div>
+      </div>
+      <div className="row-span-2 col-span-11 bg-slate-200">
+        <h1 className="m-4 p-4 text-3xl font-bold underline bg-white">
           Bienvenido a Joko Security.
         </h1>
       </div>
