cab3f8f2598960ad551c3a4a8ae9c6576480af9d
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-25 00:47:07 -0400

    Hacer que el layout se expanda toda la altura

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,32 +9,31 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <div className="grid grid-rows-3 grid-flow-col">
-      <div className="max-sm:hidden row-span-3 bg-blue-950 overflow-y-auto col-span-1 flex items-center text-slate-300">
-        <ul>
-          <li>
-            <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+    <div className="grid grid-rows-12 grid-cols-12">
+      <div className="max-sm:hidden row-span-4 row-start-1 bg-blue-950 overflow-y-auto col-span-2 py-3 text-slate-300">
+        <ul className="w-full">
+          <li className="hover:bg-blue-500">
+            <a className="px-4 text-lg hover:text-slate-100 " href="#">
               Barra de navegacion
             </a>
           </li>
-          <li>
-           <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+          <li className="hover:bg-blue-500">
+           <a className="px-4 text-lg hover:text-slate-100" href="#">
               Item 1
             </a>
           </li>
-          <li>
-            <a className="px-4 text-lg hover:text-slate-100 hover:bg-blue-500" href="#">
+          <li className="hover:bg-blue-500">
+            <a className="px-4 text-lg hover:text-slate-100" href="#">
               Item 2
             </a>
           </li>
         </ul>
       </div>
-      <div className="col-span-11">
-        <div className=""></div>
-        <div>Cabecera</div>
+      <div className="col-span-10 px-3 py-3">
+        <div>Cabecera (falta boton)</div>
       </div>
-      <div className="row-span-2 col-span-11 bg-slate-200">
-        <h1 className="m-4 p-4 text-3xl font-bold underline bg-white">
+      <div className="row-span-3 col-span-10 bg-slate-200">
+        <h1 className="m-4 p-4 text-3xl h-screen font-bold bg-white">
           Bienvenido a Joko Security.
         </h1>
       </div>
