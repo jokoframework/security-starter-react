3acb25408e67d029c19475a71bca40a46c25804b
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-29 01:22:52 -0400

    Hacer que los <li> en index.tsx sean clickeables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,6 @@ export default function Home() {
         <li>
           Boton en la cabecera para esconder y hacer aparecer el navbar.
         </li>
-        <li>
-          Hacer que el layout sea un layout de verdad (aplicarlo a diferentes rutas).
-        </li>
-        <li>
-          Hacer que los items en el navbar sean clickeables en lugar de solo el texto.
-        </li>
       </ul>
     </>
   )
