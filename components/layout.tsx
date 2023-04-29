49e362cf55ccb8802c6a5392360e85d6a24fbf5c
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-04-29 13:51:42 -0400

    Dividir componentes del Layout
    Tambien se agrego documentacion al respecto en README.md y al inicio de cada componente.

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,9 +2,35 @@ import { User, Upload, AlignJustify } from 'react-feather'
 import Link from 'next/link'
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
+  /**
+   * Layout principal del proyecto.
+   * Actualmente esta divido en 3 partes: Sidebar, Header y Content.
+   * El layout es un grid de Tailwind CSS con 12 columnas y 12 filas y renderiza todo el contenido
+   * de las paginas dentro del componente Content.
+   */
   return (
     <div className="grid grid-rows-12 grid-cols-12">
-      <div className="max-sm:hidden row-span-4 row-start-1 bg-blue-900 overflow-y-auto col-span-2 py-3 text-slate-300">
+      <Sidebar />
+      <Header />
+      <Content>
+        { children }
+      </Content>
+    </div>
+  )
+}
+
+function Sidebar() {
+  /**
+   * Sidebar del layout. 
+   * Funciona como una lista <ul></ul> con la clase overflow-y-auto donde cada elemento <li></li> representa un item dentro
+   * del sidebar. 
+   * Ademas, cada elemento que no sea el titulo del sidebar tiene un next/link alrededor del <li></li> para
+   * poder redireccionar a esa pagina.
+   * TODO: Hacer que sea colapsable.
+   */
+  return (
+    <>
+      <div className="max-md:hidden row-span-4 row-start-1 bg-blue-900 overflow-y-auto col-span-2 py-3 text-slate-300">
         <ul className="w-full">
           <li className="py-2 mx-2 font-semibold text-center">
             <Link className="px-4 text-xl hover:text-slate-50" href="/">
@@ -25,13 +51,30 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
           </Link>
         </ul>
       </div>
-      <div className="max-sm:col-span-12 col-span-10 px-4 py-4">
-        <AlignJustify className="inline-block" />
-      </div>
-      <div className="row-span-3 max-sm:col-span-12 col-span-10 bg-neutral-100">
-        <div className="m-4 p-4 h-screen bg-white">
-          { children }
-        </div>
+    </>
+  )
+}
+
+function Header() {
+  /**
+   * Cabecera de la pagina, actualmente solo contiene un icono que es clickeable para poder colapsar el sidebar.
+   * TODO: Hacer que el icono sea clickeable para colapsar el sidebar.
+   */
+  return (
+    <div className="col-span-12 md:col-span-10 row-span-1 px-4 py-4">
+      <AlignJustify className="inline-block" />
+    </div>
+  )
+}
+
+function Content({ children }: { children: React.ReactNode }) {
+  /**
+   * Representa el contenido de la pagina que esta siendo visitada actualmente.
+   */
+  return (
+    <div className="row-span-3 col-span-12 md:col-span-10 bg-neutral-100">
+      <div className="m-4 p-4 h-screen bg-white">
+        { children }
       </div>
     </div>
   )
