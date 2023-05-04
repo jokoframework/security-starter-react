4d21d44fc055b1ee998efb2731d32f12085b6e5c
Alain Vega <alainjosevz@gmail.com>
2023-05-04 00:56:31 -0400

    Cambiar disenho de pagina login #1
    * Actualice el disenho de la pagina login como lo propone la indicencia JOKO-57
    
    * Falta usar el componente Image de next (no se recomienda usar la etiqueta img de html)
    
    * Falta agregar un media query para que la foto desaparezca cuando la pagina tiene un tamano especifico
    
    * Falta hacer que al inicio no se ponga el borde y texto en rojo del input email
    
    * Investigar si Accent-color (para el background color del checkbox) es aceptado por la mayoria de navegadores

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,43 @@
 import Link from "next/link"
+import Image from "next/image"
+import { LogIn } from "react-feather"
 //Pagina de login para los usuarios.
 export default function Login() { //bg-[#FF6969] es como tailwind dice background-color: #FF6969
     return ( 
-        <> {/* TODO: Usar variables de css (no hay comentarios semanticos, bajon)*/}
-            <div className="min-h-screen min-w-full mx-auto my-auto flex flex-col justify-center items-center bg-slate-900 text-white gap-4 font-serif">
-                <form className="border flex flex-col justify-center items-center bg-slate-700 p-6 gap-4 rounded-3xl"> {/* TODO: ver donde enviar esto */}
-                    <h1 className="italic text-3xl">
-                        Inicie sesion
-                    </h1>
-                    <label className="min-w-full">Introduzca su email: 
-                        <input type="email" className="block text-black min-w-full h-8 p-2" required placeholder="ejemplo@ejemplo.com"></input>
-                    </label>
-                    <label className="min-w-full">Contraseña: 
-                        <input type="password" className="block text-black min-w-full h-8 p-2" required minLength={12} placeholder="**********"></input>
-                    </label>
-                    <button type="submit" className="bg-gray-400 rounded-2xl p-2 min-w-full">Iniciar</button>
-                    <div className="flex gap-5">
-                        <p className="hover:underline cursor-pointer" >Olvido su contraseña?</p> {/* TODO: preguntar si me tengo que preocupar por esto. */}
-                        <p className="hover:underline"><Link href="/signup">Registrarse</Link></p> 
-                    </div>
-                </form>
-                    <p>
-                        <Link href="/" className="hover:font-extrabold">Click aqui</Link> para ir a la pagina principal 
-                        {/* TODO: lo mas probable es que esto tenga que chutar */}
-                    </p>
+        <> {/* TODO: Usar variables de css */}
+            {/* falta el media query */}
+            <div className="min-h-screen min-w-full grid grid-flow-col grid-cols-2 gap-4 font-serif selection:bg-[#4f46e5] selection:text-white">
+                <div className="mx-auto my-auto flex flex-col justify-center items-center w-3/4 h-3/4">
+                    <form className=" min-h-full min-w-full flex flex-col justify-center items-center p-6 gap-4 rounded-3xl border-2"> {/* TODO: ver donde enviar esto */}
+                        {/* <Image src={""} alt="Logo"></Image> preguntar que logo quiere que ponga, o uso un icono de react-feather */}
+                        <LogIn size={150} color="#4f46e5"></LogIn>
+                        <h1 className="italic text-3xl font-black">
+                            Inicie sesion
+                        </h1>
+                        <label className="min-w-full pt-2 pb-2">Introduzca su email: {/* si esta vacio entonces que no aplique los colores */}
+                            <input type="email" className="block text-black min-w-full rounded-lg h-10 p-2 
+                            border-2 invalid:text-red-500 invalid:border-red-500" 
+                            required placeholder="ejemplo@ejemplo.com"></input>
+                        </label>
+                        <label className="min-w-full pt-2 pb-2">Contraseña: 
+                            <input type="password" className="block text-black min-w-full rounded-lg h-10 p-2 border-2" required placeholder="**********"></input>
+                        </label>
+                        <div className="min-w-full flex justify-around"> 
+                            <label className="flex items-center hover:cursor-pointer"> {/* Accent-color creo que no soportan todos los navegadores, investigar */}
+                                <input type="checkbox" className="mr-1 p-0 accent-[#4f46e5] hover:"></input>Recordarme 
+                            </label>
+                            <p className="hover:underline text-[#4f46e5]"><Link href="/signup">Registrarse</Link></p> 
+                        </div>
+                        <button type="submit" className="bg-[#4f46e5] text-white rounded-2xl p-2 min-w-full">Iniciar</button>
+                    </form>
+                        <p>
+                            <Link href="/" className="hover:font-extrabold">Click aqui</Link> para ir a la pagina principal 
+                            {/* TODO: lo mas probable es que esto tenga que chutar */}
+                        </p>
+                </div>
+                <div className="border-2 min-h-full min-w-full flex items-center justify-center">
+                    {/* <Image src={""} alt="Imagen"></Image> investigar */}
+                </div>
             </div>
         </>
     )
