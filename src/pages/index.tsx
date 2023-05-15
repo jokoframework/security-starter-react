3c62fec53cac89769a34225c0a49f65c0fb9125d
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-05-15 15:26:13 -0400

    Instalar tremor
    Se agrego adicionalmente un ejemplo de prueba a index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { Inter } from 'next/font/google'
+import { Card, Text, Metric } from '@tremor/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Card className="max-w-xs mx-auto">
+      <Text>Sales</Text>
+      <Metric>$ 34,743</Metric>
+    </Card>
       <p className="text-3xl font-bold">Bienvenido a Joko Security.</p>
       <h1 className="text-3xl">TODO:</h1>
       <ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
   ],
   theme: {
     extend: {
