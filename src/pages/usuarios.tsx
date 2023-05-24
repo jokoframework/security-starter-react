2d4c6f190c38cd16ff2566e378769194032697b3
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-05-24 17:14:46 -0400

    Agregar reset de contador de pagina al escribir en input

--- src/pages/usuarios.tsx
+++ src/pages/usuarios.tsx
@@ -68,7 +68,7 @@ export default function UsersTable() {
           <Search size={20} color="gray" className="inline-block ml-2" />
         </div>
         <input 
-          onChange={e => setNameLike(e.currentTarget.value)} 
+          onChange={e => { setNameLike(e.currentTarget.value); setPage(1) }} 
           className="block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm lg:text-lg"
           placeholder="Buscar por nombre..."
         />
