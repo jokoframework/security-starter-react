b00f04d91f0f429f1d98cbc68dc4555cffde5488
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-05-24 15:17:47 -0400

    Empezar refactor con useEffect()

--- src/pages/usuarios.tsx
+++ src/pages/usuarios.tsx
@@ -1,9 +1,10 @@
 import process from 'process'
-import { Key } from 'react'
+import { Key, useEffect } from 'react'
 import { Inter } from 'next/font/google'
 import { Search } from 'react-feather'
 import { useRouter } from 'next/router'
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'react-feather'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -86,6 +87,29 @@ export async function getServerSideProps({ query }: { query: Query }) {
  */
 export default function UsersTable({ userData, paginationData }: { userData: User[], paginationData: LinkObject }) {
   const router = useRouter()
+  const [totalCount, setTotalCount] = useState<number>(0)
+  const [nameLike, setNameLike] = useState<string>("")
+  const [page, setPage] = useState<number>(0)
+  const [limit, setLimit] = useState<number>(10)
+  const [lastPage, setLastPage] = useState<number>(0)
+  const [data, setData] = useState([])
+  useEffect(() => {7
+    fetch(`${process.env.NEXT_PUBLIC_MOCK_USER_URL}?_page=${page}&name_like=${nameLike}`)
+    .then((resp) => {
+      setTotalCount(Number(resp.headers.get('X-Total-Count')!))
+      /**
+       * Si la division es exacta, entonces la cantidad de paginas es la division.
+       * Caso contrario, la ultima pagina es la division + 1 
+       * (solo que la ultima pagina no tendra tantos elementos como limit).
+       */
+      setLastPage(Math.ceil(totalCount / limit))
+      return resp.json()
+    })
+    .then((data) => {
+      console.log(data)
+      setData(data)
+    })
+  }, [page, nameLike, totalCount, limit, lastPage])
   return (
     <>
       <div className="relative block mx-9 w-1/3">
@@ -93,7 +117,7 @@ export default function UsersTable({ userData, paginationData }: { userData: Use
           <Search size={20} color="gray" className="inline-block ml-2" />
         </div>
         <input 
-          onChange={e => router.replace(`${window.location.pathname}?name_like=${e.currentTarget.value}`)} 
+          onChange={e => setNameLike(e.currentTarget.value)} 
           className="block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm lg:text-lg"
           placeholder="Buscar por nombre..."
         />
